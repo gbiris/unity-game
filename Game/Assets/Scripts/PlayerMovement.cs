451dc279fc2654b427c5ce62b87c5a1b49ce0bf2
Gregory Biris <76085910+gbiris@users.noreply.github.com>
2023-07-05 19:34:49 +1000

    Fixed background & physics
    Added new stars background with parallax and changed the physics of rocket.

--- Game/Assets/Scripts/PlayerMovement.cs
+++ Game/Assets/Scripts/PlayerMovement.cs
@@ -13,24 +13,18 @@ public class PlayerMovement : MonoBehaviour
     public float maxSpeed; 
     private Rigidbody2D rb;
     private Vector2 startPos;
-    //public GameObject fire;
-    //public GameObject stars;
-
-    //private ParticleSystem particles;
-
-    private bool dead;
-    public static PlayerMovement Instance { get; set; }
     
+    private bool dead;
+
+    public static PlayerMovement Instance { get; set; }
+
     private void OnEnable() 
     {
         Instance = this;
         startPos = transform.position;
         rb = GetComponent<Rigidbody2D>();
         gravity = rb.gravityScale;
-        flamesEmission = flames.emission;
-        //particles = GetComponent<ParticleSystem>();
-        //particles.Stop();
-        
+        flamesEmission = flames.emission;      
     }
 
     // Update is called once per frame
@@ -39,17 +33,13 @@ public class PlayerMovement : MonoBehaviour
         Vector2 vel = rb.velocity;
         float ang = Mathf.Atan2(vel.y, x: 10) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(new Vector3(x:0, y:0, z:ang -90));
-    
-
-        //if (rb.velocity.magnitude > maxSpeed) {
-        //    rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);  
-        //}
 
         if (Input.GetKey(KeyCode.Space))
         {
             flamesEmission.enabled = true;
             rb.AddForce(Vector2.up * 1200f * gravity * Time.deltaTime);
         }
+
         if (Input.GetKeyUp(KeyCode.Space))
         {
             flamesEmission.enabled = false;
